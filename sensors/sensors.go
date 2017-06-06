a891c95fd40b46eba14a9ebae6a842341cd1ab2c
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2017-06-05 20:57:03 -0700

    add sensor code

---
+++ sensors/sensors.go
@@ -0,0 +1,179 @@
+package main
+
+import "fmt"
+
+import "os"
+import "os/signal"
+import "syscall"
+import "time"
+
+import "github.com/kidoman/embd"
+import _ "github.com/kidoman/embd/host/rpi"
+
+import "github.com/siliconbeacon/iot/sensors/si70xx"
+
+const (
+	rpiI2cBus = 1
+)
+
+func runTemperatureSensor(i2cbus embd.I2CBus, shutdown chan bool) {
+	sensor := si70xx.New(i2cbus)
+	var (
+		err      error
+		serial   string
+		model    string
+		firmware string
+	)
+	if serial, err = sensor.SerialNumber(); err != nil {
+		fmt.Println("Unable to initialize Si70xx sensor.")
+		return
+	}
+	if model, err = sensor.ModelName(); err != nil {
+		fmt.Println("Unable to initialize Si70xx sensor.")
+		return
+	}
+	if firmware, err = sensor.FirmwareVersion(); err != nil {
+		fmt.Println("Unable to initialize Si70xx sensor.")
+		return
+	}
+	fmt.Println(model, "found. Serial:", serial, "running firmware version", firmware)
+
+	// let's sample at 5Hz
+	if err = sensor.Start(200 * time.Millisecond); err != nil {
+		fmt.Println("Unable to commence sensor reads from Si70xx.")
+		return
+	}
+	readings := sensor.Readings()
+	var buffer [10]*si7021.TemperatureAndHumidity
+	sampleCount := 0
+	for {
+		select {
+		case reading := <-readings:
+			buffer[sampleCount] = reading
+			sampleCount++
+			if sampleCount == 10 {
+				fmt.Println("Got 10.")
+				// do mqtt stuff with buffer(0:sampleCount)
+				for i := range buffer {
+					fmt.Println(buffer[i].TemperatureDegreesCelsius)
+					buffer[i] = nil
+				}
+				sampleCount = 0
+			}
+		case <-shutdown:
+			sensor.Close()
+			// do mqtt stuff with buffer(0:sampleCount)
+			return
+		}
+	}
+}
+
+func main() {
+	osExit := make(chan os.Signal, 1)
+	shutdown := make(chan bool, 1)
+	signal.Notify(osExit, syscall.SIGINT, syscall.SIGTERM)
+
+	fmt.Println("Initializing Sensors...")
+	embd.InitI2C()
+	defer embd.CloseI2C()
+	i2cbus := embd.NewI2CBus(rpiI2cBus)
+
+	// start all sensors
+	go runTemperatureSensor(i2cbus, shutdown)
+
+	fmt.Println("Running...  Press Ctrl-C to exit.")
+
+	<-osExit
+	fmt.Println("Exiting.")
+	close(shutdown)
+}
+
+// func blink() {
+// 	osExit := make(chan os.Signal, 1)
+// 	shutdown := make(chan bool, 1)
+// 	signal.Notify(osExit, syscall.SIGINT, syscall.SIGTERM)
+
+// 	fmt.Println("Initializing Sensors...")
+
+// 	embd.InitGPIO()
+// 	defer embd.CloseGPIO()
+
+// 	green, _ := embd.NewDigitalPin(5)
+// 	green.SetDirection(embd.Out)
+
+// 	yellow, _ := embd.NewDigitalPin(6)
+// 	yellow.SetDirection(embd.Out)
+
+// 	blue, _ := embd.NewDigitalPin(19)
+// 	blue.SetDirection(embd.Out)
+
+// 	red, _ := embd.NewDigitalPin(26)
+// 	red.SetDirection(embd.Out)
+
+// 	fmt.Println("Blinking.  Press Ctrl-C to exit.")
+
+// 	go func() {
+// 		ticker := time.NewTicker(time.Millisecond * 200)
+// 		for {
+// 			select {
+// 			case <-ticker.C:
+// 				red.Write(embd.High)
+// 				time.Sleep(time.Millisecond * 100)
+// 				red.Write(embd.Low)
+// 			case <-shutdown:
+// 				ticker.Stop()
+// 				return
+// 			}
+// 		}
+// 	}()
+
+// 	go func() {
+// 		ticker := time.NewTicker(time.Millisecond * 350)
+// 		for {
+// 			select {
+// 			case <-ticker.C:
+// 				blue.Write(embd.High)
+// 				time.Sleep(time.Millisecond * 175)
+// 				blue.Write(embd.Low)
+// 			case <-shutdown:
+// 				ticker.Stop()
+// 				return
+// 			}
+// 		}
+// 	}()
+
+// 	go func() {
+// 		ticker := time.NewTicker(time.Millisecond * 500)
+// 		for {
+// 			select {
+// 			case <-ticker.C:
+// 				green.Write(embd.High)
+// 				time.Sleep(time.Millisecond * 250)
+// 				green.Write(embd.Low)
+// 			case <-shutdown:
+// 				ticker.Stop()
+// 				return
+// 			}
+// 		}
+// 	}()
+
+// 	go func() {
+// 		ticker := time.NewTicker(time.Millisecond * 1200)
+// 		for {
+// 			select {
+// 			case <-ticker.C:
+// 				yellow.Write(embd.High)
+// 				time.Sleep(time.Millisecond * 600)
+// 				yellow.Write(embd.Low)
+// 			case <-shutdown:
+// 				ticker.Stop()
+// 				return
+// 			}
+// 		}
+// 	}()
+
+// 	<-osExit
+// 	fmt.Println("Exiting.")
+// 	close(shutdown)
+
+// }
---
+++ sensors/si7021/si7021.go
@@ -0,0 +1,301 @@
+// Package si70xx is a driver for the Si70xx temperature / humidity sensor
+package si70xx
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/kidoman/embd"
+)
+
+const (
+	si70xxAddrDefault                = 0x40
+	si70xxCmdReadRhtRegister         = 0xE7
+	si70xxCmdMeasureRelativeHumidity = 0xF5
+	si70xxCmdMeasureTemperature      = 0xF3
+	si70xxCmdReadPreviousTemperature = 0xE0
+	si70xxCmdReset                   = 0xFE
+	si70xxRhtRegisterValue           = 0x3A
+)
+
+var (
+	si70xxCmdIdentify1       = []byte{0xFA, 0x0F}
+	si70xxCmdIdentify2       = []byte{0xFC, 0xC9}
+	si70xxCmdFirmwareVersion = []byte{0x84, 0xB8}
+	si70xxFirmwareVersions   = map[byte]string{
+		0x20: "2.0",
+		0xFF: "1.0",
+	}
+	si70xxChipModels = map[byte]string{
+		0x00: "Engineering Sample",
+		0xFF: "Engineering Sample",
+		0x0D: "Si7013",
+		0x14: "Si7020",
+		0x15: "si7021",
+	}
+)
+
+// TemperatureAndHumdity is a single reading of both Temperature
+// and Humdity values
+type TemperatureAndHumidity struct {
+	Timestamp                  time.Time
+	TemperatureDegreesCelsius  float64
+	RelativeHumidityPercentage float64
+}
+
+// Si70xx represents The Si70xx sensor (Si7013, Si7020, or Si7021)
+type Si70xx struct {
+	// Bus to communicate over.
+	Bus embd.I2CBus
+	// Addr of the sensor.
+	address byte
+
+	initialized bool
+	mu          sync.RWMutex
+
+	readings chan *TemperatureAndHumidity
+	closing  chan chan struct{}
+	serial   string
+	firmware string
+	model    string
+}
+
+// New creates a new si70xx sensor.
+func New(bus embd.I2CBus) *si70xx {
+	return &si70xx{
+		Bus:     bus,
+		address: si70xxAddrDefault,
+	}
+}
+
+// SerialNumber returns the sensor's hardware serial number
+func (d *si70xx) SerialNumber() (string, error) {
+	if err := d.setup(); err != nil {
+		return "", err
+	}
+	return d.serial, nil
+}
+
+// FirmwareVersion returns the sensor's firmware version
+func (d *si70xx) FirmwareVersion() (string, error) {
+	if err := d.setup(); err != nil {
+		return "", err
+	}
+	return d.firmware, nil
+}
+
+// ModelName returns the model name of the sensor (Si7013, Si7020, or Si7021)
+func (d *si70xx) ModelName() (string, error) {
+	if err := d.setup(); err != nil {
+		return "", err
+	}
+	return d.model, nil
+}
+
+// Humdity performs a Relative Humidity reading
+func (d *si70xx) Humidity() (float64, error) {
+	if err := d.setup(); err != nil {
+		return -1, err
+	}
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	if err := d.Bus.WriteByte(d.address, si70xxCmdMeasureRelativeHumidity); err != nil {
+		return -1, errors.New("Error asking sensor for humidity reading")
+	}
+	time.Sleep(25 * time.Millisecond)
+	reading, err := d.Bus.ReadBytes(d.address, 3)
+	if err != nil {
+		return -1, errors.New("Error reading humidity value from sensor")
+	}
+	rawValue := binary.BigEndian.Uint16(reading[0:2])
+	return float64(rawValue)*125.0/65536.0 - 6.0, nil
+}
+
+// Temperature performs a Temperature reading
+func (d *si70xx) Temperature() (float64, error) {
+	if err := d.setup(); err != nil {
+		return -1, err
+	}
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	if err := d.Bus.WriteByte(d.address, si70xxCmdMeasureTemperature); err != nil {
+		return -1, errors.New("Error asking sensor for temperature reading")
+	}
+	time.Sleep(15 * time.Millisecond)
+	reading, err := d.Bus.ReadBytes(d.address, 3)
+	if err != nil {
+		return -1, errors.New("Error reading temperature value from sensor")
+	}
+
+	return calcTemperature(reading), nil
+}
+
+// LastTemperature returns a temperature value that was measured during the last Humidity reading,
+// without waiting for the sensor to perform a new reading.
+func (d *si70xx) LastTemperature() (float64, error) {
+	if err := d.setup(); err != nil {
+		return -1, err
+	}
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	if err := d.Bus.WriteByte(d.address, si70xxCmdReadPreviousTemperature); err != nil {
+		return -1, errors.New("Error asking sensor for temperature reading")
+	}
+	reading, err := d.Bus.ReadBytes(d.address, 2)
+	if err != nil {
+		return -1, errors.New("Error reading temperature value from sensor")
+	}
+
+	return calcTemperature(reading), nil
+}
+
+// Readings is a channel that will contain sensor readings after calling Start()
+func (d *si70xx) Readings() <-chan *TemperatureAndHumidity {
+	return d.readings
+}
+
+// Start produces a stream of humidity and temperature readings
+// in the Readings() channel
+func (d *si70xx) Start(interval time.Duration) error {
+	if err := d.setup(); err != nil {
+		return err
+	}
+	if interval < 100*time.Millisecond {
+		return errors.New("Cannot sample more often than every 100 milliseconds")
+	}
+	// determine buffer size based on frequency.
+	bufferSize := 2
+	if interval < time.Second {
+		bufferSize = int(time.Second/interval) + 1
+	}
+
+	d.readings = make(chan *TemperatureAndHumidity, bufferSize)
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-ticker.C:
+				var err error
+				reading := &TemperatureAndHumidity{}
+				if reading.RelativeHumidityPercentage, err = d.Humidity(); err != nil {
+					glog.Errorf("si70xx: %v", err)
+					continue
+				}
+				reading.Timestamp = time.Now().UTC()
+				if reading.TemperatureDegreesCelsius, err = d.LastTemperature(); err != nil {
+					glog.Errorf("si70xx: %v", err)
+					continue
+				}
+				d.readings <- reading
+			case waitc := <-d.closing:
+				waitc <- struct{}{}
+				ticker.Stop()
+				close(d.readings)
+				return
+			}
+		}
+	}()
+	return nil
+}
+
+// Close stops any period reads in progress.  Call this to stop the readings that
+// Start() begins
+func (d *si70xx) Close() error {
+	if err := d.setup(); err != nil {
+		return err
+	}
+	if d.closing != nil {
+		waitc := make(chan struct{})
+		d.closing <- waitc
+		<-waitc
+	}
+
+	return nil
+}
+
+func calcTemperature(reading []byte) float64 {
+	rawValue := binary.BigEndian.Uint16(reading[0:2])
+	return float64(rawValue)*175.72/65536.0 - 46.85
+}
+
+func (d *si70xx) setup() error {
+	d.mu.RLock()
+	if d.initialized {
+		d.mu.RUnlock()
+		return nil
+	}
+	d.mu.RUnlock()
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if err := d.validate(); err != nil {
+		return err
+	}
+
+	// issue reset
+	if err := d.Bus.WriteByte(d.address, si70xxCmdReset); err != nil {
+		return errors.New("Unable to reset si70xx")
+	}
+	time.Sleep(50 * time.Millisecond)
+
+	// make sure this looks like the right sensor
+	reg, err := d.Bus.ReadByteFromReg(d.address, si70xxCmdReadRhtRegister)
+	if err != nil {
+		return err
+	}
+	if reg != si70xxRhtRegisterValue {
+		return fmt.Errorf("si70xx: Unexpected RHT Register Value: %#x", reg)
+	}
+	fmt.Printf("Reg: %#x\n", reg)
+
+	// read firmware version
+	if err := d.Bus.WriteByteToReg(d.address, si70xxCmdFirmwareVersion[0], si70xxCmdFirmwareVersion[1]); err != nil {
+		return errors.New("Unable to read Firmware Version")
+	}
+
+	firmware, err := d.Bus.ReadByte(d.address)
+	if err != nil {
+		return errors.New("Unable to read Firmware Version")
+	}
+	d.firmware = si70xxFirmwareVersions[firmware]
+
+	// read serial number and firmware version since we're connected
+	if err := d.Bus.WriteByteToReg(d.address, si70xxCmdIdentify1[0], si70xxCmdIdentify1[1]); err != nil {
+		return errors.New("Unable to read Serial Number (Part 1)")
+	}
+	serial1, err := d.Bus.ReadBytes(d.address, 8)
+	if err != nil {
+		return errors.New("Unable to read Serial Number (Part 1)")
+	}
+	if err := d.Bus.WriteByteToReg(d.address, si70xxCmdIdentify2[0], si70xxCmdIdentify2[1]); err != nil {
+		return errors.New("Unable to read Serial Number (Part 2)")
+	}
+	serial2, err := d.Bus.ReadBytes(d.address, 8)
+	if err != nil {
+		return errors.New("Unable to read Serial Number (Part 2)")
+	}
+	d.serial = fmt.Sprintf("%x%x%x%x%x%x%x%x", serial1[0], serial1[2], serial1[4], serial1[6], serial2[0], serial2[2], serial2[4], serial2[6])
+	d.model = si70xxChipModels[serial2[0]] // derived from serial number
+	d.initialized = true
+
+	return nil
+}
+
+func (d *si70xx) validate() error {
+	if d.Bus == nil {
+		return errors.New("si70xx: no i2c bus")
+	}
+	if d.address == 0x00 {
+		return fmt.Errorf("si70xx: invalid address %#x", d.address)
+	}
+	return nil
+}
