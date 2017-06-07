2c2512e9fff4eabc0308a56685fcc365f5ccf9f9
Jeremiah Wittevrongel <jeremiah@wittevrongel.fake>
2017-06-06 20:15:38 -0700

    Easy Generation of client topics

--- mqtt/topics.go
+++ mqtt/topics.go
@@ -1,5 +1,17 @@
 package mqtt
 
+import (
+	"strings"
+)
+
 const (
+	// WeatherTopicPattern is the topic pattern to listen to for readings
+	// from all weather stations
 	WeatherTopicPattern = "weather/#/readings"
 )
+
+// CreateWeatherTopic creates a specific topic for weather readings from
+// the named weather station
+func CreateWeatherTopic(station string) string {
+	return strings.Replace(WeatherTopicPattern, "#", station, 1)
+}
